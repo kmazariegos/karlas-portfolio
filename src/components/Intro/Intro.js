ca56c0f2681645b4079e18f1b9c92ba26d85b41f
Karla <mazariek@gmail.com>
2022-02-11 17:38:19 -0800

    second commit

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -4,13 +4,21 @@ import "./Intro.css"
 function Intro() {
   return (
     <div className='i'>
-        <div className='i-right'></div>
         <div className='i-left'>
             <div className='i-left-wrapper'>
-                <h2>Hello, my name is</h2>
-                <h2 className='i-name'>Karla</h2>
+                <h2 className='i-intro'>Hello, my name is</h2>
+                <h1 className='i-name'>Karla</h1>
+                <div className='i-title'>
+                    <div className='i-title-wrapper'></div>
+                        <div className='i-title-item'>Web Dev</div>
+                        <div className='i-title-item'>Web Dev</div>
+                        <div className='i-title-item'>Web Dev</div>
+                        <div className='i-title-item'>Web Dev</div>
+                </div>
             </div>
         </div>
+
+        <div className='i-right'>Right</div>
     </div>
   )
 }
