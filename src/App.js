0c94c367a9b952f4d7c402529bae15af85c5fa0e
Karla <mazariek@gmail.com>
2022-02-17 16:37:16 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { About } from './components/About/About';
 import Intro from './components/Intro/Intro';
 import ProductList from './components/ProductList/ProductList';
+import Contact from './components/Contact/Contact';
+
 
 function App() { 
   return (
@@ -9,6 +11,7 @@ function App() {
       <Intro>Intro</Intro>
       <About></About>
       <ProductList></ProductList>
+      <Contact></Contact>
     </div>
   );
 }
