9da716f5e604d533ca5eeac541b70b6982544815
Karla <mazariek@gmail.com>
2022-02-18 14:18:53 -0800

    commit

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,7 +6,12 @@ export default function Contact() {
     <div className='c'>
         <div className='c-bg'></div>
         <div className='c-wrapper'>
-            <div className='c-left'></div>
+            <div className='c-left'>
+                <h1 className='c-title'>Lets discuss the project</h1>
+                <div className='c-info'>
+                    
+                </div>
+            </div>
             <div className='c-right'></div>
         </div>
     </div>
