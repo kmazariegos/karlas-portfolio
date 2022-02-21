6828dffe3df9525d13122b26a743582d93681ed6
Karla <mazariek@gmail.com>
2022-02-21 11:49:27 -0800

    commit

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Contact.css'
+import image from '../../img/coding.png'
 
 export default function Contact() {
   return (
@@ -9,10 +10,33 @@ export default function Contact() {
             <div className='c-left'>
                 <h1 className='c-title'>Lets discuss the project</h1>
                 <div className='c-info'>
-                    
+                    <div className='c-info-item'>
+                        <img className='c-icon' src={image} alt=''></img>
+                        Email
+                    </div>
+                    <div className='c-info-item'>
+                        <img className='c-icon' src={image} alt=''></img>
+                        Address
+                    </div>
+                    <div className='c-info-item'>
+                        <img className='c-icon' src={image} alt=''></img>
+                        Phone Number
+                    </div>
                 </div>
             </div>
-            <div className='c-right'></div>
+            <div className='c-right'>
+                <p className='c-desc'>
+                    <b>What's your story?</b>
+                    Here is the story
+                </p>
+                <form>
+                    <input type='text' placeholder='Name' name='user_name'></input>
+                    <input type='text' placeholder='Subject' name='user_subject'></input>
+                    <input type='text' placeholder='Email' name='user_email'></input>
+                    <textarea rows="5" placeholder='Message' name='message'/>
+                    <button>Submit</button>
+                </form>
+            </div>
         </div>
     </div>
   )
