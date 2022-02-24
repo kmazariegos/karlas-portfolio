15b9edf627682db362d403a16f21499e3fb90718
Karla <mazariek@gmail.com>
2022-02-24 12:28:20 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { About } from './components/About/About';
 import Intro from './components/Intro/Intro';
 import ProductList from './components/ProductList/ProductList';
 import Contact from './components/Contact/Contact';
+import Toggle from './components/Toggle/Toggle';
 
 
 function App() { 
   return (
     <div className="App">
+      <Toggle></Toggle>
       <Intro>Intro</Intro>
       <About></About>
       <ProductList></ProductList>
---
+++ src/components/Toggle/Toggle.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Toggle.css"
+import image from '../../img/coding.png'
+
+function Toggle() {
+  return (
+    <div className='t'>Toggle
+        <img className='t-icon' alt='' src={image}></img>
+        <img className='t-icon' alt='' src={image}></img>
+        <div className='t-button'></div>
+    </div>
+  )
+}
+
+export default Toggle
