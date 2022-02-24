308e41a6b0f6aa17d1bb9decc56760b096605544
Karla <mazariek@gmail.com>
2022-02-24 14:45:59 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Intro from './components/Intro/Intro';
 import ProductList from './components/ProductList/ProductList';
 import Contact from './components/Contact/Contact';
 import Toggle from './components/Toggle/Toggle';
+import { useState } from 'react';
 
 
 function App() { 
+  const [dark, setDark] = useState(false)
   return (
     <div className="App">
-      <Toggle></Toggle>
-      <Intro>Intro</Intro>
-      <About></About>
-      <ProductList></ProductList>
-      <Contact></Contact>
+      <Toggle setDark={setDark}></Toggle>
+      <Intro dark={dark}>Intro</Intro>
+      <About dark={dark}></About>
+      <ProductList dark={dark}></ProductList>
+      <Contact dark={dark}></Contact>
     </div>
   );
 }
