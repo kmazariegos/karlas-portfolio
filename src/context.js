1d7a5f1985e13b114b6b520136557c6c1f6d436d
Karla <mazariek@gmail.com>
2022-02-25 12:53:31 -0800

    commit

---
+++ src/context.js
@@ -0,0 +1,10 @@
+import { createContext } from 'react';
+
+export const ThemeContext = createContext()
+
+
+const INITAL_STATE = {darkMode: false};
+
+const themeReducer =(state, action) => {
+    
+}
