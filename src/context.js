2a92d617fc1574a2d433b1d80606d7e92ddf4d04
Karla <mazariek@gmail.com>
2022-02-25 15:05:29 -0800

    commit

--- src/context.js
+++ src/context.js
@@ -1,10 +1,30 @@
 import { createContext } from 'react';
+import { ThemeProvider } from 'react-bootstrap';
 
 export const ThemeContext = createContext()
 
 
 const INITAL_STATE = {darkMode: false};
 
+
 const themeReducer =(state, action) => {
-    
+    switch(action.type){
+        
+        case "TOGGLE":
+        return {darkMode: !state.darkMode};
+        
+        default: 
+        return state;
+    }
+}
+
+
+export const themeProvider = (props) => {
+    const [state, dispatch] = userReducer(themeReducer, INITAL_STATE);
+
+    return(
+        <ThemeProvider value={(state, dispatch)}>
+            {props.children}
+        </ThemeProvider>
+    )
 }
