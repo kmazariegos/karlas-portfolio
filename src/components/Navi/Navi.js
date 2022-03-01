a15df60512327a7fa6c15d9fbbf224d764ee978c
Karla <mazariek@gmail.com>
2022-02-28 16:01:11 -0800

    commit

--- src/components/Navi/Navi.js
+++ src/components/Navi/Navi.js
@@ -4,7 +4,18 @@ import "./Navi.css"
 function Navi() {
   return (
     <div className='n'>
-    
+      <a class="n-logo" href='/'>
+      <div>karla</div>
+      </a>
+      
+      <a class="n-logo-p" href='/'>
+      <div>projects</div>
+      </a>
+
+      <a class="n-logo-p" href='/'>
+      <div>resume</div>
+      </a>
+
     </div>
   )
 }
