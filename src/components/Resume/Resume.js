f8c63dca11b89b1439ccfc33fc01d681c6b7bdf7
Karla <mazariek@gmail.com>
2022-03-06 15:30:49 -0800

    commit

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -8,29 +8,28 @@ function Resume() {
         <Toggle></Toggle>
 
         <div className='l-left-wrapper'>
-            <h1 class="resume-title">Experience</h1>
-            <h2 className="Main-title">Karla Mazariegos</h2>
+
+            <h1 class="resume-title">About</h1>
             <h2 className="Second-title">Highly entrepreneurial problem-solver and software enthusiast motivated by bringing concepts to full fruition. 
             Proven ability to successfully analyze an organization's business requirements, identify potential opportunities, and develop innovative solutions to enhance product revenue and user experience. 
             Valuable in a variety of industries and fast-paced environments.</h2>
-          </div>
 
-          <div className='r-right-wrapper'>
             <h1 class="resume-title">Education</h1>
             <h2 className="Main-title">Boston College</h2>
-            <h2 className="Second-title">B.A., Communications & Marketing – Boston, MA (2015)</h2>
-          </div>
+            <h2 className="Second-title">B.A., Communications & Marketing - Boston, MA (2015)</h2>
+            <br></br>
+            <h2 className="Main-title">Springboard</h2>
+            <h2 className="Second-title">Software Engineering Immersive - Los Angeles, CA (2020)</h2>
 
-          <div className='l-left-wrapper'>
-            <h1 class="resume-title">Experience</h1>
-            <h2 className="Main-title">Karla Mazariegos</h2>
-            <h2 className="Second-title">Education</h2>
-          </div>
+            <h1 class="resume-title">Skills & Abilities</h1>
+            <h2 className="Second-title">JavaScript | React | Redux | HTML | CSS | NodeJS | Express | SQL | PostgreSQL | MongoDB | Flask | Restful API Development | Mocha | Chai | GitHub 
+            | Jira | Salesforce | Postman | Tableau | Excel | Entrepreneurship | E-commerce | Brand Development | Operations Management | Strategic Planning 
+            | Supply Chain Management | Amazon Seller Central | Shopify | SEO | Google Analytics  | Ad Campaign Management</h2>
           
-          <div className='r-right-wrapper'>
-            <h1 class="resume-title">Resume</h1>
-            <h2 className="Main-title">Karla Mazariegos</h2>
-            <h2 className="Second-title">Education</h2>
+            <h1 class="resume-title">Experience</h1>
+            <h2 className="Main-title">Software Engineering Student, Springboard</h2>
+            <h2 className="Second-title">Completed a coding boot camp that covered key aspects of software engineering.</h2>
+          
           </div>
 
 
