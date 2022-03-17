fab59f1dcbf53ffb3f9c68c4f411bfd53d80d79d
Karla <mazariek@gmail.com>
2022-03-16 21:31:49 -0700

    commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,9 +7,9 @@ function Footer() {
         <h1 className='f-title'>Find more of me on:</h1>
 
         <div class="container-2 w-container">
-        <a href='wwww.karlamazariegos.com' target="_blank" className="footer-links">LinkedIn</a>
-        <a href='wwww.karlamazariegos.com' className="footer-links">Resume</a>
-        <a href='wwww.karlamazariegos.com' target="_blank" className="footer-links">Github</a>
+        <a href='https://www.linkedin.com/in/karla-mazariegos/' target="https://www.linkedin.com/in/karla-mazariegos/" className="footer-links">LinkedIn</a>
+        <a href='https://docs.google.com/document/d/1ExU4i1drW4wDXOy4zlH7KSwozJ-pR2nuUx_oUslFF_4/edit?usp=sharing' target="https://docs.google.com/document/d/1ExU4i1drW4wDXOy4zlH7KSwozJ-pR2nuUx_oUslFF_4/edit?usp=sharing" className="footer-links">Resume</a>
+        <a href='https://github.com/kmazariegos' target="https://github.com/kmazariegos" className="footer-links">Github</a>
         </div>
 
     </div>
--- src/components/SecondProject/SecondProject.js
+++ src/components/SecondProject/SecondProject.js
@@ -15,7 +15,7 @@ function SecondProject() {
 
       <div className='s-left'>
           <div className='s-right-wrapper'>
-              <h1 className='s-name'>Bus Tracking App</h1>
+              <h1 className='s-name'>Furniture Ecommerce Store</h1>
               <p className='s-desc'>
               Reimagining the way you chat with others on this collaborative iOS app
               </p>
