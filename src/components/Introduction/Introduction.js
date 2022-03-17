4b2ba200771a2b0cc460bf1e2f41f51e8e8c9a3e
Karla <mazariek@gmail.com>
2022-03-16 22:17:04 -0700

    commit

--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import "./Introduction.css"
+import "./Introduction.css" 
 
 function Introduction() { 
   return (
 <div className='long-intro'>
             <div class="hero-content text">
             <h1 class="heading">Hello! I'm Karla</h1>
-            <p class="hero-paragraph">A product designer with an empathetic approach to designing polished and engaging user experiences.</p>
+            <p class="hero-paragraph">A web developer with an empathetic approach to building polished and engaging user experiences.</p>
             <div className='introductionButton sideIntro'>
-            <a href="project" data-w-id="Button" class="introduction-button">See my work</a>
+            <a href="../ProjectPage/ProjectPage.js" data-w-id="Button" class="introduction-button">See my work</a>
             </div>
             </div>
 
