e83a1d32d76435182095e81d7aa3e4e1bca6f1b3
Karla <mazariek@gmail.com>
2022-05-05 20:08:44 -0700

    Commit

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -8,12 +8,7 @@ function Resume() {
         <Toggle></Toggle>
 
         <div className='l-left-wrapper'>
-
-            <h1 class="resume-title">About</h1>
-            <h2 className="Second-title">Highly entrepreneurial problem-solver and software enthusiast motivated by bringing concepts to full fruition. 
-            Proven ability to successfully analyze an organization's business requirements, identify potential opportunities, and develop innovative solutions to enhance product revenue and user experience. 
-            Valuable in a variety of industries and fast-paced environments.</h2>
-
+        
             <h1 class="resume-title">Education</h1>
             <h2 className="Main-title">Boston College</h2>
             <h2 className="Second-title">B.A., Communications & Marketing - Boston, MA (2015)</h2>
